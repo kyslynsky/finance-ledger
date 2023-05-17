5ee04d1a911cd01a0b1d57ac54aafa6539f7d9a2
kyslynsky <kyslynsky@gmail.com>
2023-05-17 11:21:15 +0300

    add lazy loading for img tag

--- src/helpers/picturesFactory.tsx
+++ src/helpers/picturesFactory.tsx
@@ -6,7 +6,7 @@ export const picturesFactory = (images: IPicture[]): JSX.Element[] => {
       <source srcSet={(i.srcwebp.concat(" 1x"), i.src.concat(" 2x"))} />
       <source srcSet={(i.srcjpg.concat(" 1x"), i.srcjpg2x.concat(" 2x"))} />
 
-      <img src={i.srcjpg} alt={i.alt} />
+      <img src={i.srcjpg} alt={i.alt} loading="lazy" />
     </picture>
   ));
 };
